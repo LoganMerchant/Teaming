d05189554b4d5770b58b52081947772a0f262df3
Logan Merchant <merchantclogan@gmail.com>
2020-10-01 14:18:43 -0600

    add test for getBasicInfo method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,17 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Test', 4, 'blank@gmail.com');
+    const employee = new Employee('test', 4, 'blank@blank.com');
 
-    expect(employee.name).toBe('Test');
+    expect(employee.name).toBe('test');
     expect(employee.employeeId).toEqual(expect.any(Number));
     expect(employee.email).toMatch('@');
+});
+
+test('checks that the basic info for an employee is being returned', () => {
+    const employee = new Employee('test', 4, 'blank@blank.com');
+
+    expect(employee.getBasicInfo()).toHaveProperty('name');
+    expect(employee.getBasicInfo()).toHaveProperty('id');
+    expect(employee.getBasicInfo()).toHaveProperty('email');
 });

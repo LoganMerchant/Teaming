b72cfe9a5c0cb21205a5bdd5a3ee0f469ce26ba2
Logan Merchant <merchantclogan@gmail.com>
2020-10-01 15:22:44 -0600

    update Manager to inherit Employee, add test to ensure that

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,16 +1,18 @@
 const Manager = require('../lib/Manager.js');
 
-jest.mock('../lib/Employee.js');
-
 test('checks that an office number is being returned', () => {
-    const manager = new Manager(1111);
+    const manager = new Manager('test', 9999, 'test@test.com', 1111);
 
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(1111);
 });
 
 test('checks that all manager info is being returned', () => {
-    const manager = new Manager(1111);
+    const manager = new Manager('test', 9999, 'test@test.com', 1111);
 
-    expect(manager.getBasicInfo()).toEqual(expect.any(Object));
-    expect(manager.getOfficeNumber).toEqual(expect.any(Number));
+    expect(manager.getBasicInfo()).toMatchObject({
+        name: 'test',
+        employeeId: 9999,
+        email: 'test@test.com',
+    });
+    expect(manager.getOfficeNumber()).toEqual(1111);
 })

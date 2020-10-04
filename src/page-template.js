ec33b4240077e5b84203d31939600235f80b6cc0
Logan Merchant <merchantclogan@gmail.com>
2020-10-03 23:56:47 -0600

    update page-template to account for empty intern/engineer arrays

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,55 @@
+const generateEngineers = engineerArr => {
+    if (!engineerArr) {
+        return '';
+    };
+
+    return `
+        ${engineerArr
+            .map(({ name, employeeId, email, github }) => {
+                return `
+                <div class="card bg-secondary col-12 col-md-6 col-lg-3">
+                    <div class="card-body">
+                        <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
+                        <h6 class="card-subtitle p-3 bg-primary">Engineer</h6>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Employee ID: ${employeeId}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${github}">${github}</a></li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+            })
+        }
+    `;
+};
+
+const generateInterns = internArr => {
+    if (!internArr) {
+        return '';
+    };
+
+    return `
+        ${internArr
+            .map(({ name, employeeId, email, school }) => {
+                return `
+                <div class="card bg-secondary col-12 col-md-6 col-lg-3">
+                    <div class="card-body">
+                        <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
+                        <h6 class="card-subtitle p-3 bg-primary">Intern</h6>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Employee ID: ${employeeId}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                            <li class="list-group-item">School: ${school}</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+            })
+        }
+    `;
+};
+
 const generateTeam = (managerArr, engineerArr, internArr) => {
     return `
     <!DOCTYPE html>
@@ -22,60 +74,28 @@ const generateTeam = (managerArr, engineerArr, internArr) => {
                 ${managerArr
                     .map(({ name, employeeId, email, officeNumber }) => {
                         return `
-                            <div class="card bg-secondary col-12 col-md-6 col-lg-3">
-                                <div class="card-body">
-                                    <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
-                                    <h6 class="card-subtitle p-3 bg-primary">Manager</h6>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Employee ID: ${employeeId}</li>
-                                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-                                        <li class="list-group-item">Office Number: ${officeNumber}</li>
-                                    </ul>
-                                </div>
+                        <div class="card bg-secondary col-12 col-md-6 col-lg-3">
+                            <div class="card-body">
+                                <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
+                                <h6 class="card-subtitle p-3 bg-primary">Manager</h6>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Employee ID: ${employeeId}</li>
+                                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                                    <li class="list-group-item">Office Number: ${officeNumber}</li>
+                                </ul>
                             </div>
+                        </div>
                         `;
                     })
                 }
             </section>
 
             <section id="engineers" class="row p-1">
-                ${engineerArr
-                    .forEach(({ name, employeeId, email, github }) => {
-                        return `
-                            <div class="card bg-secondary col-12 col-md-6 col-lg-3">
-                                <div class="card-body">
-                                    <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
-                                    <h6 class="card-subtitle p-3 bg-primary">Engineer</h6>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Employee ID: ${employeeId}</li>
-                                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-                                        <li class="list-group-item">GitHub: <a href="https://github.com/${github}">${github}</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        `;
-                    })
-                }
+                ${generateEngineers(engineerArr)}
             </section>
 
             <section id="managers" class="row p-1">
-                ${internArr
-                    .forEach(({ name, employeeId, email, school }) => {
-                        return `
-                            <div class="card bg-secondary col-12 col-md-6 col-lg-3">
-                                <div class="card-body">
-                                    <h5 class="card-subtitle p-3 bg-primary">${name}</h5>
-                                    <h6 class="card-subtitle p-3 bg-primary">Intern</h6>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Employee ID: ${employeeId}</li>
-                                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
-                                        <li class="list-group-item">School: ${school}</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        `;
-                    })
-                }
+                ${generateInterns(internArr)}
             </section>
 
         </main>
